6f1f7a61f05508103ee5592894b5fab85c0da561
venkata-sainadh <136240129+venkata-sainadh@users.noreply.github.com>
2024-12-21 14:51:52 +0530

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-white">
       <div className="container-fluid">
         <a className="navbar-brand" href="/"> 
-          <img src="logo.png" alt="Logo" width="40" /> Sara
+          <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2Fprofile-user-round-white-icon-symbol-png--686236061986678319%2F&psig=AOvVaw3kftGGjakqlTpi9hqU0rIO&ust=1734859209263000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJi1wcbEuIoDFQAAAAAdAAAAABAE" alt="Logo" width="40" /> Sara
         </a>
         <div className="input-group search-bar">
           <input type="text" className="form-control" placeholder="Search" />
